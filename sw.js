63d4aa7008c3cd89f08430d1c2a225fb730e6982
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-11 20:08:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("push", (e) => {
   var options = {
     body: "Yay it works.",
-    icon: "images/icon.png",
-    badge: "images/badge.png",
+    icon: "img/icon.png",
+    badge: "img/badge.png",
   };
 
   e.waitUntil(self.registration.showNotification("test", options));
