eeca5f543b3415a76f504ca0df0893e6311ec0e0
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-11 22:13:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
 self.addEventListener("push", (e) => {
   var options = {
-    body: "Yay it works.",
-    icon: "img/icon.png",
-    badge: "img/badge.png",
-  };
+  {
+  "body": "Did you make a $1,000,000 purchase at Dr. Evil...",
+  "icon: "img/icon.png",
+  "vibrate": [200, 100, 200, 100, 200, 100, 400],
+  "tag": "request",
+  "actions": [
+    { "action": "yes", "title": "Yes", "icon": "images/yes.png" },
+    { "action": "no", "title": "No", "icon": "images/no.png" }
+  ]
+};
 
   e.waitUntil(self.registration.showNotification("test", options));
 });
