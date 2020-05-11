dd572acadc3ea1e501890396db6afdae1c0da212
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-11 22:19:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("push", (e) => {
   var options = {
-  
   "body": "Did you make a $1,000,000 purchase at Dr. Evil...",
   "icon": "img/icon.png",
+  "imageUrl": "https://stimg.cardekho.com/images/carexteriorimages/360x240/Mahindra/Mahindra-Alturas-G4/1547728970602/047.jpg",
   "vibrate": [200, 100, 200, 100, 200, 100, 400],
   "tag": "request",
   "actions": [
