d0d3f419a1a57e8b8cd3e6dfbde46a821341fd90
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-27 00:15:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,3 +13,25 @@ var options = {
 
   e.waitUntil(self.registration.showNotification("test", options));
 });
+
+
+self.addEventListener('push', function(event) {
+  event.waitUntil(
+    self.registration.pushManager.getSubscription().then(function(subscription) {
+      fetch('https://localhost:44363/api/ExternalClient/Notification', {
+        method: 'get'
+      })
+      .then(function(response) { return response.json(); })
+      .then(function(data) {
+        self.registration.showNotification(data.title, {
+          body: data.body,
+          icon: 'img/icon.png',
+          image: data.image
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    })
+  );
+});
