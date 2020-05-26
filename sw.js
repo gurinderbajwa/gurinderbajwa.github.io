3df406ee95d2fa39fd28dcfbfbadd698537017ef
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-27 00:16:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,18 +3,6 @@ self.addEventListener('install', function (event) {
  self.skipWaiting();
 });
 
-self.addEventListener("push", (e) => {  
-var options = {
-  "body": "Did you make a $1,000,000 purchase at Dr. Evil...",
-  "icon": "img/icon.png",
-  "image": "https://stimg.cardekho.com/images/carexteriorimages/360x240/Mahindra/Mahindra-Alturas-G4/1547728970602/047.jpg",
-  "badge": "https://stimg.cardekho.com/images/carexteriorimages/360x240/Mahindra/Mahindra-Alturas-G4/1547728970602/047.jpg"
-};
-
-  e.waitUntil(self.registration.showNotification("test", options));
-});
-
-
 self.addEventListener('push', function(event) {
   event.waitUntil(
     self.registration.pushManager.getSubscription().then(function(subscription) {
