2cc81025ba73f87ef69d24c3dba9ab9177232853
gurinderbajwa <gurinderbajwa@yahoo.com>
2020-05-27 23:15:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,24 +2,16 @@
 self.addEventListener('install', function (event) {
  self.skipWaiting();
 });
+self.addEventListener("push", event => {
+  if (event.data) {
+    var payload = event.data.json();
+    var title = payload.title;
+    var options = {
+      body: payload.body,
+      icon: payload.icon,
+      image: payload.image
+    };
 
-self.addEventListener('push', function(event) {
-  event.waitUntil(
-    self.registration.pushManager.getSubscription().then(function(subscription) {
-      fetch('https://localhost:44363/api/ExternalClient/Notification', {
-        method: 'get'
-      })
-      .then(function(response) { return response.json(); })
-      .then(function(data) {
-        self.registration.showNotification(data.title, {
-          body: data.body,
-          icon: 'img/icon.png',
-          image: data.image
-        });
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    })
-  );
+    event.waitUntil(self.registration.showNotification(title, options));
+  }
 });
